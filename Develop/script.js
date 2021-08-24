5511d9e18e79619551851a93b43c8799f815d6ad
Superstar <Lbryttney@yahoo.com>
2021-08-23 21:50:05 -0500

    new changes

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
+function generatePassword() {
+var passwordLength = prompt("Choose a password length ranging from 8 to 128 characters?");
+console.log(passwordLength);
+var passwordLowercase = confirm ("Would you like to include lowercase characters?");
+console.log(passwordLowercase);
+var passwordUppercase = confirm ("Would you like to include uppercase characters?");
+console.log(passwordUppercase);
+var passwordNumeric = confirm ("Would you like to include numeric characters?");
+console.log(passwordNumeric);
+var passwordSpecial = confirm ("Would you like to include special characters?");
+console.log(passwordSpecial);
+alert("Here's your password: ******");
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  console.log(password);
   passwordText.value = password;
 
 }

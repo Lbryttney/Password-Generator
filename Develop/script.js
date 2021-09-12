01de91831e038af267843347ea6d6e9926bb3920
Superstar <Lbryttney@yahoo.com>
2021-09-12 17:45:04 -0500

    problem solving,stuck at password push

--- Develop/script.js
+++ Develop/script.js
@@ -1,27 +1,49 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
-
-function generatePassword() {
-var passwordLength = prompt("Choose a password length ranging from 8 to 128 characters?");
-console.log(passwordLength);
-var passwordLowercase = confirm ("Would you like to include lowercase characters?");
-console.log(passwordLowercase);
-var passwordUppercase = confirm ("Would you like to include uppercase characters?");
-console.log(passwordUppercase);
-var passwordNumeric = confirm ("Would you like to include numeric characters?");
-console.log(passwordNumeric);
-var passwordSpecial = confirm ("Would you like to include special characters?");
-console.log(passwordSpecial);
-alert("Here's your password: ******");
-}
 // Write password to the #password input
 function writePassword() {
+  //Password criteria prompt: character length
+  var passwordLength = window.prompt('Choose a password length between 8 and 128 characters?', '');
+
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    console.log (parseInt(passwordLength));
+  } else {
+    window.alert('Please enter a valid password length!');
+  }
+
+  //Password criteria confirmation: character types
+  var passwordLowercase = window.confirm('Would you like to include lowercase characters?','');
+  console.log(passwordLowercase);
+
+  var passwordUppercase = window.confirm('Would you like to include uppercase characters?','');
+  console.log(passwordUppercase);
+
+  var passwordNumeric = window.confirm('Would you like to include numeric characters?','');
+  console.log(passwordNumeric);
+
+  var passwordSpecial = window.confirm('Would you like to include special characters?','');
+  console.log(passwordSpecial);
+
+  //Gather all password criteria in one array
+  var passwordCriteria = [passwordLength, passwordLowercase, passwordUppercase, passwordNumeric, passwordSpecial];
+  console.log(passwordCriteria);
+
   var password = generatePassword();
+
+  function generatePassword() {
+   var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+   var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+   var numeric = '0123456789';
+   var special = '!@#$%^&*()_+~`|}{[]\:;?><,./-='; 
+
+   
+  }
   var passwordText = document.querySelector("#password");
-  console.log(password);
+
   passwordText.value = password;
+
+
 
 }
 
